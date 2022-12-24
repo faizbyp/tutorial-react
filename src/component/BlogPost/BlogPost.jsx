1b36a74f68ff11a40241d3e4b548d5b222a6caed
faizbyp <faizbyp@gmail.com>
2022-12-24 21:59:54 +0700

    http delete method

--- src/component/BlogPost/BlogPost.jsx
+++ src/component/BlogPost/BlogPost.jsx
@@ -8,14 +8,7 @@ export class BlogPost extends Component {
     post: []
   }
 
-  componentDidMount(){
-    // fetch('https://jsonplaceholder.typicode.com/posts')
-    //   .then(response => response.json())
-    //   .then(json => {
-    //     this.setState({
-    //       post: json
-    //     })
-    //   })
+  getPostAPI = () => {
     axios.get('http://localhost:3004/posts')
       .then((res) => {
         this.setState({
@@ -24,16 +17,33 @@ export class BlogPost extends Component {
       })
   }
 
+  handleRemove = (data) => {
+    axios.delete(`http://localhost:3004/posts/${data}`)
+    .then((res) => {
+      this.getPostAPI()
+    })
+  }
+
+  componentDidMount(){
+    // fetch('https://jsonplaceholder.typicode.com/posts')
+    //   .then(response => response.json())
+    //   .then(json => {
+    //     this.setState({
+    //       post: json
+    //     })
+    //   })
+    this.getPostAPI()
+  }
+
   render() {
     return (
       <Fragment>
         <p className='section-title'>BlogPost</p>
         {
           this.state.post.map(post => {
-            return <Post key={post.id} title={post.title} desc={post.body} />
+            return <Post key={post.id} data={post} remove={this.handleRemove} />
           })
         }
-        
       </Fragment>
     )
   }
